8accef1d90c5db795394c9cec971e051cfdc1cba
MyPC <edinlaw@live.com>
2019-05-18 15:06:20 +0200

    Using comment and post stream in the same time

--- init.py
+++ init.py
@@ -34,124 +34,151 @@ reddit = praw.Reddit(client_id=keys['client_id'],
 # Get already fetched submissions and comments
 syncedPosts = []
 syncedComments = []
+botComments = []
 for result in sql_con.execute(
         "SELECT `comment_id` FROM `comments`"):
     syncedComments.append(result[0])
 
 for result in sql_con.execute(
-        "SELECT `submission_id` FROM `submissions`"):
+        "SELECT `submission_id`,`comment_id` FROM `submissions`"):
     syncedPosts.append(result[0])
+    botComments.append(result[1])
 
 
 # Functions
 def addSyncedComment(commentId):
-    syncedComments.append(reply.id)  # Add to synced comments
+    syncedComments.append(commentId)  # Add to synced comments
     sql_temp.execute(  # Add comment to synced comments in database
         "INSERT INTO comments (comment_id) VALUES ('" + str(commentId) + "')")
+
+
+def addBotComment(commentId):
+    botComments.append(commentId)
+    sql_temp.execute(  # Add comment to synced comments in database
+        "INSERT INTO submissions (comment_id) VALUES ('" + str(commentId) + "')")
 
 
 def addUserToComment(userId, commentId):
     sql_temp.execute(  # Add user to synced users for this submission
         "INSERT INTO users (user_id, submission_id) VALUES (?,?)",
-        (reply.author.id,
-         dataId[0]))
+        (userId,
+         commentId))
+
 
 def skipPost(postId):
     sql_temp.execute(  # Post is removed, change data so it will be skipped from now on
         "UPDATE submissions SET skip = 1 WHERE submission_id = '" + str(skipPost) + "';")
 
 
+def submissions_and_comments(sub, **kwargs):
+    results = []
+    results.extend(sub.new(**kwargs))
+    results.extend(sub.comments(**kwargs))
+    results.sort(key=lambda post: post.created_utc, reverse=True)
+    return results
+
+
+def isPost(check):
+    if hasattr(check, "link_author"):
+        return False
+    else:
+        return True
+
+
 # Subreddit Work
 subreddit = reddit.subreddit(config['subreddit'])
-for submission in subreddit.stream.submissions(pause_after=-1):
-    print("Searching for posts...")
-    if submission is not None:  # Check if a post is found
+dataStream = praw.models.util.stream_generator(lambda **kwargs: submissions_and_comments(subreddit, **kwargs))
+
+for data in dataStream:
+    if isPost(data):
+        print("New Post")
+        submission = data  # I didn't wanna rewrite it
         if submission.id not in syncedPosts:  # Check if submission wasn't synced yet
-            print("New Post Found")
-            print("\tCommenting: ")
+            print("\tPost is new!")
+            print("\t\tCommenting: ")
             replyObj = submission.reply(config['comment_text'])
-            print("\t\tComment Added.")
+            print("\t\t\tComment Added.")
             replyObj.mod.distinguish(sticky=True)
-            print("\t\tComment Stickied.")
+            print("\t\t\tComment Stickied.")
             sql_con.execute(
                 "INSERT INTO submissions (submission_id, comment_id) VALUES (?,?)",
                 (submission.id,
                  replyObj.id))
-            sql_config.commit()
-            print("\t\tAdded to database.\n\n")
-    else:  # No new posts, do the comment thingy
-        print("No new posts found, starting comment processing...")
-        print("\tGetting submissions and comment ids from database.")
-        dataList = sql_con.execute(
-            "SELECT `submission_id`,`comment_id`,`popular`,`unpopular` FROM submissions WHERE skip IS 0")
-        for dataId in dataList:  # For every dataId (submission Data)
-            popular = dataId[2]
-            unpopular = dataId[3]
-            alreadyVoted = []
-            print("\tGetting reddit post data: ", end="")
-            postData = reddit.submission(id=dataId[0])  # Request post Data
-            print("Done")
-            if postData.banned_by is None:  # Check if post is removed (returns None if it isn't)
-                print("\tGetting users who already voted: ", end="")
-                for userLoad in sql_temp.execute(  # Get users who already voted on this submission
-                        "SELECT `user_id` FROM users WHERE submission_id IS '" + dataId[0] + "'"):
-                    alreadyVoted.append(userLoad[0])
-                print("Done")
+            syncedPosts.append(submission.id)
+            addBotComment(replyObj.id)
+            print("\t\t\tAdded to database.\n\n")
+    else:
+        comment = data
+        print("New Comment")
+        if comment.id not in syncedComments:
+            print("\tComment is new!")
+            addSyncedComment(comment.id)
+            if comment.parent_id[3:] in botComments:  # Check if the reply is to a bot comment
+                print("\t\tProcessing: ")
+                sql_con.execute(
+                    "SELECT `popular`,`unpopular` FROM submissions WHERE submission_id IS '"
+                    + comment.link_id[3:] + "'")
+                dbFetch = sql_con.fetchone()
+                popular = dbFetch[0]
+                unpopular = dbFetch[1]
+                oldVotes = popular + unpopular
 
-                print("\tComment Data.\n\t\tGetting comment data: ", end="")
-                commentData = reddit.comment(id=dataId[1])  # Request comment data.
-                print("Done\n\t\tRefreshing Comments: ", end="")
-                commentData.refresh()  # Refresh comments (For some reason this is required now)
-                print("Done")
-                try:
-                    print("\t\t\tReply Processing: ")
-                    oldVotes = popular + unpopular
-                    for reply in commentData.replies:  # Loop thru all replies
-                        if reply.id not in syncedComments and reply.author.id not in alreadyVoted and reply.author_flair_css_class is not "f":  # Check if reply is not synced yet, author didn't vote yet, and is not banned from voting with a hidden CSS class
-                            addSyncedComment(reply.id)  # Add comment to synced comments
-                            if reply.author.id != postData.author.id:  # Check if the reply is not written by the author of the submission
-                                if "unpopular" in reply.body.lower():
-                                    print("\t\t\t\tUnpopular")
-                                    unpopular += 1
-                                    addUserToComment(reply.author.id, dataId[0])
-                                elif "popular" in reply.body.lower():
-                                    popular += 1
-                                    print("\t\t\t\tPopular")
-                                    addUserToComment(reply.author.id, dataId[0])
-                            else:
-                                print("\t\t\t\tCommenting on own submission.")
-                                reply.mod.remove("You cannot vote on your own submission.")
-                                reply.author.message("Comment Removed",
-                                                     "Please don't vote on your own submissions.\n\n#**This message is from a bot. Your reply will not be read.**")
-                    if oldVotes != (popular + unpopular):  # Data changed
-                        # Edit the comment to show new data
-                        print("\t\t\tChanging comment body: ", end="")
-                        commentData.edit(
-                            config[
-                                'comment_text'] + f'\n\nCurrent Votes:\n\nPopular|Unpopular\n:--|:--\n{popular}|{unpopular}')
-                        sql_temp.execute(  # Update popular/unpopular in the database
-                            "UPDATE submissions SET unpopular = " + str(unpopular) +" ,popular = " + str(popular) +
-                            " WHERE submission_id = '" + str(dataId[0]) + "';")
-                        print("Done")
-                    else:
-                        print("\t\t\t\tNo new replies.")
-                except Exception as e:
-                    print(e)
+                if not comment.is_submitter:  # Not OP
+                    print("\t\t\tCommenter is NOT Op")
+                    sql_con.execute(  # Get users who already voted on this submission
+                        "SELECT * FROM users WHERE submission_id IS '" + comment.link_id[3:] + "' AND user_id IS '"
+                        + comment.author_fullname[3:] + "'")
+                    if sql_con.fetchone() is None:  # User didn't vote
+                        voted = False
+                        if "unpopular" in comment.body.lower():
+                            unpopular += 1
+                            voted = True
+                            print("\t\t\tVoted: Unpopular")
+                        elif "popular" in comment.body.lower():
+                            popular += 1
+                            voted = True
+                            print("\t\t\tVoted: Popular")
 
-                minutes = (datetime.utcnow() - datetime.fromtimestamp(postData.created_utc)).total_seconds() / 60
-                if popular + unpopular >= config['votes'] and minutes > config['minutes']:
-                    print("\t\t\tPost fulfils requirements for voting")
-                    popularity = round(popular / (popular + unpopular))
-                    postData.mod.flair(f"Voted {popularity} Popular")
-                    skipPost(dataId[0])
+                        if voted:
+                            addUserToComment(comment.author_fullname[3:], comment.link_id[3:])
+                            print("\t\t\tEditing bot comment to show new data: ", end="")
+                            botCommentData = reddit.comment(id=comment.parent_id[3:])
+                            botCommentData.edit(
+                                config[
+                                    'comment_text'] + f'\n\nCurrent Votes:\n\nPopular|Unpopular\n:--|:--\n{popular}|{unpopular}')
+                            print("Done")
+                            sql_temp.execute(  # Update popular/unpopular in the database
+                                "UPDATE submissions SET unpopular = " + str(unpopular) + " ,popular = " + str(popular) +
+                                " WHERE submission_id = '" + str(comment.link_id[3:]) + "';")
+                            print("\t\t\tAdded to database")
 
-                    if popularity >= 70:
-                        postData.report(
-                            "BotReport: Opinion has " + str(popular) + " votes against " + str(
-                                unpopular) + " votes.")
+                            postData = reddit.submission(id=comment.link_id[3:])  # Request post Data
+                            minutes = (datetime.utcnow() - datetime.fromtimestamp(
+                                postData.created_utc)).total_seconds() / 60
+                            if popular + unpopular >= config['votes'] and minutes > config['minutes']:
+                                print("\t\t\tPost fulfils requirements.")
+                                popularity = round(popular / (popular + unpopular))
+                                postData.mod.flair(f"Voted {popularity} Popular")
+                                skipPost(comment.link_id)
+
+                                if popularity >= 70:
+                                    print("\t\t\tPost is popular opinion.\n\t\t\tReporting: ", end="")
+                                    postData.report(
+                                        "BotReport: Opinion has " + str(popular) + " votes against " + str(unpopular) + " votes.")
+                                    print("Done")
+
+                    else:  # User voted already
+                        print("\t\t\tUser Voted Already")
+                else:  # Remove comment and tell OP not to vote to own posts
+                    print("\t\t\tCommenter IS OP. Processing: ")
+                    comment.mod.remove("You cannot vote on your own submission.")
+                    print("\t\t\tComment Removed.")
+                    comment.author.message("Comment Removed",
+                                           "Please don't vote on your own submissions.\n\n#**This message is from a bot. Your reply will not be read.**")
+                    print("\t\t\tOP Notified")
+                    addUserToComment(comment.author_fullname[3:], comment.link_id[3:])
             else:
-                print("Post Removed")
-                skipPost(dataId[0])
+                print("\t\tComment is not a reply to the bot.")
 
-            print("\n\n")
-            sql_config.commit()  # commit database changes
+    sql_config.commit()  # commit database changes
+    print("Changes committed\n")
