ef77b72f97d77c6c8da52400843339fbab59e20a
MyPC <edinlaw@live.com>
2019-08-21 01:29:42 +0200

    Revert "Updated gitIgnore"
    This reverts commit ee8f1f56f3e4e86c5da61a359a284597d94957df.

--- keys.py
+++ keys.py
@@ -1,7 +1,7 @@
 keys = dict(
-    client_id="60muAevhOOZM5Q",
-    client_secret="4dIE6_kGjELcHrjT1PDu18pL5hU",
-    user_agent="Personal use Script",
-    username="mredinlaw_bot",
-    password="cnuN9@6ldZ@3",
+    client_id="",
+    client_secret="",
+    user_agent="",
+    username="",
+    password="",
 )
